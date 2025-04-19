9dba1d4dd52cbb3101a295eae3508a14086dafd9
bijonl <bijon2591@gmail.com>
2025-04-19 11:51:07 -0400

    footer config "

---
+++ components/footer/social-icons.php
@@ -0,0 +1,26 @@
+<div class="social-wrapper">
+    <a aria-label="new window will open to email precious works" 
+        href="mailto:<?php echo $email_address ?>" 
+        target="_blank"
+        rel="noopener"
+        title="email precious works">
+        <img 
+            class="social-icon"
+            src="<?php echo get_template_directory_uri() ?>/assets/images/mail-icon.png"
+            alt="email-icon"
+            title="email-icon"
+        />
+    </a>
+    <a aria-label="new window/tab will open to go to LinkedIn" 
+        href="<?php echo $linkedin_url ?>" 
+        target="_blank"
+        rel="noopener"
+        title="go to bijons linkedin page">
+        <img 
+            class="social-icon"
+            src="<?php echo get_template_directory_uri() ?>/assets/images/linkedin-icon.png"
+            alt="linkedin-icon"
+            title="linkedin-icon"
+        />
+    </a>
+</div>
--- footer.php
+++ footer.php
@@ -1,8 +1,41 @@
-    <footer class="site-footer py-4 mt-auto border-top">
-        <div class="container text-center">
-            <p class="mb-0">
-                Footer
-            </p>
+<?php $linkedin_url = 'https://www.linkedin.com/in/bijonlb/'; 
+$email_address = 'bijonlb.dev@gmail.com';
+$copyright_text = '&copy '.get_the_date('Y').' Precious Works'; ?>
+    
+    <footer class="site-footer mt-auto">
+        <div class="container footer-container text-center">
+            <div class="back-to-top-row row">
+                <div class="col back-to-top-col text-end">
+                    <a aria-label="go to top of page" 
+                        href="#" 
+                        title="button to go back to top">
+                        <img 
+                            class="social-icon"
+                            src="<?php echo get_template_directory_uri() ?>/assets/images/chevron-up-filled.png"
+                            alt="back-to-top-icon"
+                            title="back-to-top-icon"
+                        />
+                    </a>
+                </div>
+            </div>
+            <div class="row footer-content-row align-items-center">
+                <div class="col-sm-4 copyright-col text-start">
+                    <p  class="small-text">
+                        <?php echo $copyright_text ?>
+                    </p>
+                </div>
+
+                <div class="col-sm-4 logo-col">
+                    <img 
+                        src="<?php echo get_template_directory_uri() ?>/assets/images/pw_mark.png"
+                        alt="email-icon"
+                        title="email-icon"
+                    />
+                </div>
+                <div class="col-sm-4 social-col text-end">
+                    <?php include(locate_template('components/footer/social-icons.php')); ?>
+                </div>
+            </div>
         </div>
     </footer>
     <?php wp_footer(); ?>
